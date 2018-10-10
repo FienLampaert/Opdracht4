b646914a8344a2912231dc6b3c5723c491a5d907
gebruiker <gebruiker@gebruikers-imac.local>
2018-10-10 21:45:36 +0100

    Labels toegekend

---
+++ Opdracht4/ClockHelper.swift
@@ -0,0 +1,11 @@
+//
+//  ClockHelper.swift
+//  Opdracht4
+//
+//  Created by gebruiker on 10/10/2018.
+//  Copyright © 2018 gebruiker. All rights reserved.
+//
+
+import Foundation
+
+
--- Opdracht4/ViewController.swift
+++ Opdracht4/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblHet: [UILabel]!
+    @IBOutlet var lblIs: [UILabel]!
+    @IBOutlet var lblVijf: [UILabel]!
+    @IBOutlet var lblTien: [UILabel]!
+    @IBOutlet var lblKwart: [UILabel]!
+    @IBOutlet var lblOver: [UILabel]!
+    @IBOutlet var lblVoor: [UILabel]!
+    @IBOutlet var lblHalf: [UILabel]!
+    @IBOutlet var lblTwee: [UILabel]!
+    @IBOutlet var lblAcht: [UILabel]!
+    @IBOutlet var lblEen: [UILabel]!
+    @IBOutlet var lblVier: [UILabel]!
+    @IBOutlet var lblZeven: [UILabel]!
+    @IBOutlet var lblElf: [UILabel]!
+    @IBOutlet var lblTwaalf: [UILabel]!
+    @IBOutlet var lblZes: [UILabel]!
+    @IBOutlet var lblDrie: [UILabel]!
+    @IBOutlet var lblVijf2: [UILabel]!
+    @IBOutlet var lblNegen: [UIView]!
+    @IBOutlet var lblTien2: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
