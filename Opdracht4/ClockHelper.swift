72d308e875fe9acb355efdf81ef7a2a98677bf57
student <student@vh-pjrh092016.local>
2018-10-11 10:44:05 +0200

    Gewerkt aan ClockHelper

--- Opdracht4/ClockHelper.swift
+++ Opdracht4/ClockHelper.swift
@@ -8,4 +8,141 @@
 
 import Foundation
 
-
+class ClockHelper{
+    var tekst: [String]
+    
+    init(vc: ViewController){
+        tekst = ["HET","IS"]
+    }
+    
+    func setUur(uur: Int){
+        switch (uur){
+        case 1:
+            tekst.append("EEN");
+            break;
+        case 2:
+            tekst.append("TWEE");
+            break;
+        case 3:
+            tekst.append("DRIE");
+            break;
+        case 4:
+            tekst.append("VIER");
+            break;
+        case 5:
+            tekst.append("VIJFu");
+            break;
+        case 6:
+            tekst.append("ZES");
+            break;
+        case 7:
+            tekst.append("ZEVEN");
+            break;
+        case 8:
+            tekst.append("ACHT");
+            break;
+        case 9:
+            tekst.append("NEGEN");
+            break;
+        case 10:
+            tekst.append("TIENu");
+            break;
+        case 11:
+            tekst.append("ELF");
+            break;
+        case 12:
+            tekst.append("TWAALF");
+            break;
+        default:
+            break;
+        }
+        
+    }
+    
+    func setMinuten(minuten: Int, u: Int){
+        switch minuten {
+        case 5:
+            tekst.append("VIJF");
+            tekst.append("OVER");
+            setUur(uur: u)
+            break
+        case 10:
+            tekst.append("TIEN");
+            tekst.append("OVER");
+            setUur(uur: u)
+            break
+        case 15:
+            tekst.append("KWART");
+            tekst.append("OVER");
+            setUur(uur: u)
+            break
+        case 20:
+            tekst.append("TIEN");
+            tekst.append("VOOR");
+            tekst.append("HALF");
+            let uHalf = u + 1
+            setUur(uur: uHalf)
+            break
+        case 25:
+            tekst.append("VIJF");
+            tekst.append("VOOR");
+            tekst.append("HALF");
+            let uHalf = u + 1
+            setUur(uur: uHalf)
+            break;
+        case 30:
+            tekst.append("HALF");
+            let uHalf = u + 1
+            setUur(uur: uHalf)
+            break;
+        case 35:
+            tekst.append("VIJF");
+            tekst.append("OVER");
+            tekst.append("HALF");
+            let uHalf = u + 1
+            setUur(uur: uHalf)
+            break;
+        case 40:
+            tekst.append("TIEN");
+            tekst.append("OVER");
+            tekst.append("HALF");
+            let uHalf = u + 1
+            setUur(uur: uHalf)
+            break;
+        case 45:
+            tekst.append("KWART");
+            tekst.append("VOOR");
+            setUur(uur: u)
+            break;
+        case 50:
+            tekst.append("TIEN");
+            tekst.append("VOOR");
+            setUur(uur: u)
+            break;
+        case 55:
+            tekst.append("VIJF");
+            tekst.append("VOOR");
+            setUur(uur: u)
+            break;
+        default:
+            break;
+        }
+    }
+    
+    func getMinuten(minuten: Int) -> Int{
+        var min = 0
+        if(minuten % 5 != 0){
+            let rest = minuten % 5
+            min = minuten - rest
+        }
+        else{
+            min = minuten
+        }
+        
+        return min
+    }
+    
+    func getTime() -> [String]{
+        return tekst;
+    }
+}
--- Opdracht4/ViewController.swift
+++ Opdracht4/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ch = ClockHelper()
+    
     @IBOutlet var lblHet: [UILabel]!
     @IBOutlet var lblIs: [UILabel]!
     @IBOutlet var lblVijf: [UILabel]!
@@ -28,14 +30,129 @@ class ViewController: UIViewController {
     @IBOutlet var lblZes: [UILabel]!
     @IBOutlet var lblDrie: [UILabel]!
     @IBOutlet var lblVijf2: [UILabel]!
-    @IBOutlet var lblNegen: [UIView]!
+    @IBOutlet var lblNegen: [UILabel]!
     @IBOutlet var lblTien2: [UILabel]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tijd(ViewControler self)
     }
-
-
+    
+    func tijd(){
+        let date = Date()
+        let calendar = Calendar.current
+        let minuten = calendar.component(.minute, from: date)
+        let min = ch.getMinuten(minuten: minuten)
+        let uur = calendar.component(.hour, from: date)
+        ch.setMinuten(minuten: min, u: uur)
+        let time = ch.getTime()
+        
+        time.forEach { woord in
+            switch (woord){
+            case "HET":
+                for label in self.lblHet {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "IS":
+                for label in self.lblIs {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "VIJF":
+                for label in self.lblVijf {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "TIEN":
+                for label in self.lblTien {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "KWART":
+                for label in self.lblKwart {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "OVER":
+                for label in self.lblOver {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "VOOR":
+                for label in self.lblVoor {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "HALF":
+                for label in self.lblHalf {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "TWEE":
+                for label in self.lblTwee {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "ACHT":
+                for label in self.lblAcht {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "EEN":
+                for label in self.lblEen {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "Vier":
+                for label in self.lblVier {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "ZEVEN":
+                for label in self.lblZeven {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "ELF":
+                for label in self.lblElf {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "TWAALf":
+                for label in self.lblTwaalf {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "ZES":
+                for label in self.lblZes {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "DRIE":
+                for label in self.lblDrie {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "VIJFu":
+                for label in self.lblVijf2 {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "NEGEN":
+                for label in self.lblNegen {
+                    label.textColor = UIColor.black
+                }
+                break
+            case "TIENu":
+                for label in self.lblTien2 {
+                    label.textColor = UIColor.black
+                }
+                break
+            default:
+                break
+            }
+        }
+    }
 }
-
